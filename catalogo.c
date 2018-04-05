e1553fb734e4d6b97030808e8115acdae96fe2f7
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2018-04-05 13:57:49 +0300

    Cria protótipo de funções

--- catalogo.c
+++ catalogo.c
@@ -17,7 +17,24 @@ typedef struct cliente
 {
     int id;
     char nome[MAX];
-};
+}t_cliente;
+
+//prototipos de funcoes
+char menu();
+void cadastrar_cliente();
+void cadastrar_filme();
+void listar_filmes();
+void listar_clientes();
+t_filme *obter_filme(FILE *arq_filmes, int id_filme);
+t_cliente *obter_cliente(FILE *arq_clientes, int id_cliente);
+void pesquisar_filme();
+void pesquisar_cliente();
+int existe_filme(FILE *arq_filmes, int id_filme);
+int existe_cliente(FILE *arq_clientes, int id_cliente);
+void atualizar_filmes(FILE *arq_filmes, t_filme *filme_alugado);
+void alugar_filme();
+void excluir_filme();
+int str_somente_numeros(char str[]);
 
 int main()
 {
